3e4a6b0e7ecb4c19e24a194c7bd7a03ea71f4f54
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-05-05 20:45:42 +0300

    [#163629]

--- src/main/java/ru/job4j/collection/DepDeskComp.java
+++ src/main/java/ru/job4j/collection/DepDeskComp.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class DepDeskComp implements Comparator <String> {
     @Override
     public int compare(String o1, String o2) {
-        int tmp = o2.split ("/") [0] .compareTo (o1.split ("/") [0]);
-        return tmp != 0 ? tmp: o1.compareTo (o2);
+        int tmp = o2.split ("/") [0] .compareTo (o1.split ("/") [0]); //сравнение до первого слэша
+        return tmp != 0 ? tmp: o1.compareTo (o2); //сравнение в лексиграфическом порядке
         }
     }
 
--- src/main/java/ru/job4j/lambda/FunctionCount.java
+++ src/main/java/ru/job4j/lambda/FunctionCount.java
@@ -7,11 +7,16 @@ import java.util.function.Function;
 public class FunctionCount {
     List <Double> diapason(int start, int end, Function <Double, Double> func) {
        FunctionCount function = new FunctionCount();
-        for(int i=start; i <= end; i++) {
-           diapason(start, end, func).add(func.apply((double) end));
-        }
-        return new ArrayList<>();
-       }
+       //Function <Double, Double> function1 = Double::doubleValue;
+          for(int i = start; i <= end; i++) {
+              if ( i != 0 ){
+                  diapason(start, end, func).add(func.apply((double) i));
+                  List list = new ArrayList<>();
+
+              }
+           }
+        return new ArrayList <>();
     }
+}
 
 
