5a1409be8cf21c6b4076dc7fee126caf7d8cf5f5
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-05-07 23:38:13 +0300

    [#163630]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,23 +1,25 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList <Person> persons = new ArrayList<Person>();
+    private ArrayList <Person> persons = new ArrayList <Person>();
 
-    public void add(Person person) {
+    public void add( Person person ) {
         this.persons.add(person);
     }
 
-    public ArrayList<Person> find(String key) {
-      /**  Predicate <Person> combine = ...;
-        ArrayList<Person> result = new ArrayList<>();
-        for(Person persons : persons) {
-            if(combine.test(persons)) {
-                result.add(persons);
+    public ArrayList <Person> find(String key) {
+        Predicate <Person> combine1 = person -> persons.contains(key);
+        Predicate <Person> combine2 = person -> persons.contains(person);
+        Predicate <Person> combine = combine1.or(combine2);
+        ArrayList <Person> result = new ArrayList <>();
+        for(Person person : persons) {
+            if (combine.test(person)){
+                result.add(person);
             }
         }
-        return result;*/
-      return null;
+        return result;
     }
 }
