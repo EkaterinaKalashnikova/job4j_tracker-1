deb5782c9a68a5b6c838c905757e6efd36adb0cb
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-05-09 15:56:20 +0300

    [#163630]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -11,9 +11,11 @@ public class PhoneDictionary {
     }
 
     public ArrayList <Person> find(String key) {
-        Predicate <Person> combine1 = person -> persons.contains(key);
-        Predicate <Person> combine2 = person -> persons.contains(person);
-        Predicate <Person> combine = combine1.or(combine2);
+        Predicate <Person> combine1 = person -> person.getName().equals(key);
+        Predicate <Person> combine2 = person -> person.getSurname().equals(key);
+        Predicate <Person> combine3 = person -> person.getPhone().equals(key);
+        Predicate <Person> combine4 = person -> person.getAddress().equals(key);
+        Predicate <Person> combine = combine1.or(combine2).or(combine3).or(combine4);
         ArrayList <Person> result = new ArrayList <>();
         for(Person person : persons) {
             if (combine.test(person)){
