4fa9a2a66b1c7f3b060fe5fc4c3a3589fee079a8
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-05-09 16:19:42 +0300

    [#163630]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList <Person> persons = new ArrayList <Person>();
+    private ArrayList<Person> persons = new ArrayList<Person>();
 
-    public void add( Person person ) {
+    public void add(Person person) {
         this.persons.add(person);
     }
 
-    public ArrayList <Person> find(String key) {
-        Predicate <Person> combine1 = person -> person.getName().equals(key);
-        Predicate <Person> combine2 = person -> person.getSurname().equals(key);
-        Predicate <Person> combine3 = person -> person.getPhone().equals(key);
-        Predicate <Person> combine4 = person -> person.getAddress().equals(key);
-        Predicate <Person> combine = combine1.or(combine2).or(combine3).or(combine4);
-        ArrayList <Person> result = new ArrayList <>();
-        for(Person person : persons) {
-            if (combine.test(person)){
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> combine1 = person -> person.getName().equals(key);
+        Predicate<Person> combine2 = person -> person.getSurname().equals(key);
+        Predicate<Person> combine3 = person -> person.getPhone().equals(key);
+        Predicate<Person> combine4 = person -> person.getAddress().equals(key);
+        Predicate<Person> combine = combine1.or(combine2).or(combine3).or(combine4);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
