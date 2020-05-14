5fedfa7b2f1a3e714569575237db48fe81b02bea
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-05-14 23:39:04 +0300

    [#163638]

--- src/main/java/stream/Address.java
+++ src/main/java/stream/Address.java
@@ -45,18 +45,19 @@ public class Address {
     }
 
     @Override
-    public boolean equals( Object o ) {
-        if ( this == o ) return true;
-        if ( o == null || getClass() != o.getClass() ) return false;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Address address = (Address) o;
         return getHome() == address.getHome() &&
                 getApartment() == address.getApartment() &&
-                Objects.equals(getCity() ,address.getCity()) &&
-                Objects.equals(getStreet() ,address.getStreet());
+                Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet());
     }
 
     @Override
-    public int hashCode( ) {
+    public int hashCode()
+    {
         return Objects.hash(getCity() ,getStreet() ,getHome() ,getApartment());
     }
 }
--- src/main/java/stream/Profiles.java
+++ src/main/java/stream/Profiles.java
@@ -1,25 +1,19 @@
 package stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    private Address address;
-
-    public Address getAddress( ) {
-        return address;
-    }
-
     public List <Profiles> collect( List<Profiles> profiles) {
        List<Profiles> profilesList = profiles.stream()
                .map(Profiles::getAddress)
-               .distinct()
-               .sorted(Comparator.naturalOrder().)
                .collect(Collectors.toList());
 
-
         return profilesList;
+    }
+
+    private static <Address> Profiles getAddress(Profiles profiles) {
+        return profiles;
     }
 }
 
