061edf90a901a137f9d88b07e8bc7ccd9ac89a78
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-05-16 10:21:45 +0300

    [#163637]

--- src/test/java/stream/SchoolTest.java
+++ src/test/java/stream/SchoolTest.java
@@ -1,41 +1,48 @@
 package stream;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SchoolTest {
+    private List<Student> list = new ArrayList <>();
+
+    @Before
+    public void setUp() throws Exception {
+      new Student(25,"Ivanov");
+      new Student(98,"Petrov");
+      new Student(54,"Sidorov");
+    }
 
     @Test
     public void groupOfStudents10a() {
         School school = new School();
-        List <Student> result = Collections.unmodifiableList(school
-                .collect(,p -> p.getScore() > 7 && p.getScore() < 100));
+        List <Student> result = school.collect(list, student -> student.getScore() >= 70
+                && student.getScore() <= 100);
         List <Student> expected = new ArrayList<Student>();
-                assertThat((result., is(expected));
+        assertThat(result, is(expected));
     }
 
     @Test
     public void groupOfStudents10b() {
         School school = new School();
-        List <Student> result = school
-                .collect(, p -> p.getScore() > 5 && p.getScore() < 70);
+        List <Student> result = school.collect(list, student -> student.getScore() >= 50
+                && student.getScore() < 50);
         List <Student> expected = new ArrayList<Student>();
-        assertThat((result, is(expected));
+        assertThat(result, is(expected));
     }
 
     @Test
     public void groupOfStudents10c() {
         School school = new School();
-        List <Student> result = school
-                .collect(, p -> p.getScore() > 0 && p.getScore() < 50);
+        List <Student> result = school.collect(list, student -> student.getScore() >= 0
+                && student.getScore() < 50);
         List <Student> expected = new ArrayList<Student>();
-        assertThat((result, is(expected));
+        assertThat(result, is(expected));
     }
 }
