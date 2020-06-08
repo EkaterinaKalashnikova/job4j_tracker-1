c07c5afe99deb3ad5d8cd2cf8bb6cb8157a75c16
EkaterinaVestia11 <Kalashnikovakaterina477@gmail.com>
2020-06-08 22:34:19 +0300

    Банковские переводы. [#163747]

--- src/main/java/ru/jobj4/bank/BankService.java
+++ src/main/java/ru/jobj4/bank/BankService.java
@@ -24,10 +24,13 @@ public class BankService {
      */
     public void addAccount(String passport, Account account) {
         List <Account> accounts = this.users.get(findByPassport(passport));
-        if (accounts != null) {
-            int index = accounts.indexOf(account);
-            if (index == -1){ //есть ли счета у клиента нет
-             accounts.add(account); //добавляем
+        User user = findByPassport(passport);
+        if (user != null) {
+            if (accounts != null) {
+                int index=accounts.indexOf(account);
+                if (index == -1) { //есть ли счета у клиента нет
+                    accounts.add(account); //добавляем
+                }
             }
         }
     }
